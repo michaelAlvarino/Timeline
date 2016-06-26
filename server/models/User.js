cc536d8fd4e3d5ba613f49f607026b5ef1aeb079
Walter Tan <waltertan12@gmail.com>
2016-06-26 08:43:58 -0700

    Be more consistent with tabbing. Fix promise

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,8 @@
 'use strict';
 /* globals module, require */
 
-const BaseModel = require('./BaseModel.js');
 const bcrypt	= require('bcrypt');
-const config 	= require('../../config.json');
-const model 	= require('objection').Model;
+const model		= require('objection').Model;
 
 // Private instance variables
 var emailRegex = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
@@ -30,8 +28,9 @@ class User extends model {
 		};
     }
 
-	static validateUser(userAttributes){
-		var errors = [], validations = _getValidations(userAttributes), key;
+	static validateUser(userAttributes) {
+		var errors = [], validations = _getValidations(userAttributes), 
+			key;
 
 		for (key in validations) {
 			if (validations.hasOwnProperty(key) && validations[key]) {
@@ -39,32 +38,20 @@ class User extends model {
 			}
 		}
 
-		return new Promise (fulfill, reject) {
+		return new Promise((fulfill, reject) => {
 			if (errors.length === 0) {
 				var date = new Date().toISOString();
 				fulfill({
-					email: 			userAttributes.email,
+					email:			userAttributes.email,
 					passwordDigest: _createPasswordDigest(userAttributes.password),
-					userType: 		userAttributes.userType,
-					createdDate: 	date,
-					updatedDate: 	date
+					userType:		userAttributes.userType,
+					createdDate:	date,
+					updatedDate:	date
 				});
 			} else {
 				reject(errors);
 			}
-		};
-		// if(!userAttributes.password || !userAttributes.email || (userAttributes.email.search(emailRegex) === -1)){// search takes the regex and returns the location or -1 on failure
-		// 	return false;
-		// }
-		
-		// var dt = new Date().toISOString();
-		// userAttributes.passwordDigest = _createPasswordDigest(userAttributes.password);
-		// userAttributes.createdDate = dt;
-		// userAttributes.updatedDate = dt;
-		
-		// delete userAttributes.password;
-		
-		// return userAttributes;
+		});
 	}
 }
 
@@ -79,20 +66,20 @@ var _getValidations = (userAttributes) => {
 		userAttributes.userType = 'user';
 	}
 
-	var password    = userAttributes.password,
-		email 		= userAttributes.email,
+	var password	= userAttributes.password,
+		email		= userAttributes.email,
 		userType	= userAttributes.userType;
 
 	return {
-		'Password required': 						!password,
-		'Passowrd must be greater than 8 chars': 	password && password.length < 9,
-		'Password requires 1 lower case letter': 	password && password.search(/[a-z]/) === -1,
-		'Password requires 1 upper case letter': 	password && password.search(/[A-Z]/) === -1,
+		'Password required':						!password,
+		'Passowrd must be greater than 8 chars':	password && password.length < 9,
+		'Password requires 1 lower case letter':	password && password.search(/[a-z]/) === -1,
+		'Password requires 1 upper case letter':	password && password.search(/[A-Z]/) === -1,
 		'Password requires 1 number': 				password && password.search(/[0-9]/) === -1,
 		'Email required':							!email,
-		'Email is not valid': 						email && email.search(emailRegex) === -1,
-		'User type not valid': 						['user', 'moderator', 'admin', 'theRock'].indexOf(userType) < 0
-	}
+		'Email is not valid':						email && email.search(emailRegex) === -1,
+		'User type not valid':						['user', 'moderator', 'admin', 'theRock'].indexOf(userType) < 0
+	};
 };
 
 module.exports = User;
