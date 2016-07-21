990213aa21b71d266554163f2eb479e626a30c2f
Walter Tan <waltertan12@gmail.com>
2016-07-20 22:51:55 -0700

    RegexTrie: Write insert method

---
+++ server/helpers/RegexTrie.js
@@ -0,0 +1,73 @@
+const paramsRegex 	= /(.*)(:[a-zA-Z0-9\-_]+\b)\((.*)\)/;
+const regexRegex 	= /[?^{}()|[\]\\]/;
+
+/**
+ * @constructor
+ * @param {string}	partition	
+ * @param {boolean}	regex
+ * @param {boolean}	leaf
+ */
+var RegexTrieNode = function (partition, regex, leaf) {
+	this.partition 	= partition || '';
+	this.regex		= regex 	|| false;
+	this.leaf		= leaf 		|| false;
+	this.children	= {};
+};
+
+/**
+ * @constructor
+ */
+var RegexTrie = function () {
+	this.root 		= new RegexTrieNode();
+	this.patterns 	= {};
+};
+
+/**
+ * Inserts a regex pattern into the trie
+ * 
+ * @param	{string}	pattern	A URL or path
+ * @return	{RegexTrie}			Fluent setter
+ */
+RegexTrie.prototype.insert = function (pattern) {
+	if (typeof pattern !== 'string') { 
+		throw 'Pattern must be a string!';
+	}
+
+	if (!this.patterns.hasOwnProperty(pattern)) {
+		var pathPartitions = pattern.split('/');
+		this._insert(pathPartitions);
+	}
+
+	return this;
+};
+
+/**
+ * 
+ * @param  {string[]} pathPartitions [description]
+ * @return {[type]}                [description]
+ */
+RegexTrie.prototype._insert = function (pathPartitions) {
+	var currentNode 		= this.root,
+		pathPartitionLength = pathPartitions.length,
+		index, partition, leaf, regex, matches;
+console.log(pathPartitions);
+	for (index = 0; index < pathPartitionLength; index++) {
+		partition 	= pathPartitions[index];
+		regex		= partition.search(regexRegex) !== -1;
+		leaf		= index === pathPartitionLength - 1;
+console.log(partition.match(paramsRegex));
+		// Ehhhhhhh
+		if (regex && (matches = partition.match(paramsRegex))) {
+			console.log(matches);
+			partition = matches[3];
+		}
+
+		if (!currentNode.children.hasOwnProperty(partition)) {
+			currentNode.children[partition] = new RegexTrieNode(partition, regex, leaf);
+		}
+
+		currentNode = currentNode.children[partition];
+	}
+};
+
+module.exports = RegexTrie;
