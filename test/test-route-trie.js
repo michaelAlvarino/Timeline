832ebc7c5534295cd4b51ab30e7202492ad50b91
Walter Tan <waltertan12@gmail.com>
2016-07-26 19:04:27 -0700

    RouteTrie: Add test coverage for RouteTrie

---
+++ test/test-route-trie.js
@@ -0,0 +1,47 @@
+/* globals process, require, describe, it */
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const RouteTrie     = require('../server/helpers/RouteTrie');
+const chai          = require('chai');
+const should        = chai.should();
+
+describe('User', () => {
+    describe('#insert', () => {
+        it('should store a route', () => {
+            var trie    = new RouteTrie(),
+                route   = '/api/users';
+
+            trie.insert(route);
+            trie.patterns.should.have.property('/api/users');
+        });
+    });
+
+    describe('#match', () => {
+        it('should return true if a pattern has been stored in the trie', () => {
+            var trie    = new RouteTrie(),
+                route   = '/never/gonna/give/you/up';
+
+            trie.insert(route);
+            trie.match(route).should.equal(true);
+        });
+
+        it('should return false if a pattern has not been stored in the trie', () => {
+            var trie    = new RouteTrie(),
+                route   = '/never/gonna/let/you/down';
+
+            trie.insert(route);
+            trie.match('/never/gonna/run/around/and/desert/you').should.equal(false);
+        });
+
+        it('should match basic regex', () => {
+            var trie    = new RouteTrie(),
+                route   = '/api/users/:id(\\d+)',
+                url     = '/api/users/3';
+
+            trie.insert(route);
+            trie.match(url).should.equal(true);
+        });
+    });
+});
