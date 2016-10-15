7cd7f8022fbef7bce4a65cf9e08858cabd9fbac6
Walter Tan <wtan@coursehero.com>
2016-10-14 21:49:48 -0700

    Setup frontend environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,65 @@
-// TODO: All of it
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "8888";
+
+// global css
+loaders.push({
+    test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
+    loaders: [
+        'style?sourceMap',
+        'css'
+    ]
+});
+// local scss modules
+loaders.push({
+    test: /[\/\\]public[\/\\].*\.scss/,
+    exclude: /node_modules/,
+    loaders: [
+        'style?sourceMap',
+        'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+        'sass'
+    ]
+});
 
 module.exports = {
-    entry: "./public/entry.js",
+    entry: [
+        `webpack-dev-server/client?http://${HOST}:${PORT}`,
+        'webpack/hot/only-dev-server',
+        './public/entry.jsx'
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
     output: {
         path: path.join(__dirname, 'public'),
         filename: "bundle.js"
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['react-hot', 'jsx', 'babel'],
-                exclude: /node_modules/
-            }
-        ]
-    }
+        loaders
+    },
+    devServer: {
+        contentBase: "./public",
+        // do not print bundle build stats
+        noInfo: true,
+        // enable HMR
+        hot: true,
+        // embed the webpack-dev-server runtime into the bundle
+        inline: true,
+        // serve index.html in place of 404 responses to allow HTML5 history
+        historyApiFallback: true,
+        port: PORT,
+        host: HOST
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
 };
